cd5ac07113f9d735e0a8dbea6afb7d9d4232af89
Anwesh43 <anweshthecool0@gmail.com>
2020-07-29 01:35:59 +0530

    adding Schema class

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 class SchemaKey {
-    
-    key 
+
+    key
     type
     options = {}
     constructor(key) {
     	this.key = key
-    } 
+    }
 
     setType(type) {
     	this.type = type
-    	return this 
+    	return this
     }
-    
+
     addOption(key, value) {
        this.options[key] = value
        return this
@@ -27,5 +27,29 @@ class SchemaKey {
     }
 }
 
+class Schema {
 
-module.exports = {SchemaKey}
+    properties = {}
+
+    addProperty(key, type, options) {
+        const schemaKey = SchemaKey.create(key).setType(type)
+        if (options) {
+            Object.keys(options).forEach((option) => {
+                schemaKey.addOption(option, options[option])
+            })
+        }
+        const property = schemaKey.build()
+        this.properties = {...this.properties, ...property}
+        return this
+    }
+
+    build() {
+        return {"properties" : this.properties}
+    }
+
+    static create() {
+        return new Schema()
+    }
+}
+
+module.exports = {SchemaKey, Schema}
